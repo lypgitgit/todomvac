eeeb679bd01e4a415f34532309da4fbdc3f8b3c3
lypgitgit <lypheidabiye@163.com>
2019-08-28 19:15:06 +0800

    lyp

---
+++ src/store/getters.js
@@ -0,0 +1,7 @@
+const count = state => state.rowData
+
+export default count
+
+// const count = function count (state) {
+//    return state.count
+// }
---
+++ src/store/state.js
@@ -0,0 +1,8 @@
+const state = {
+  count: 0,
+  rowData: [
+
+  ]
+}
+
+export default state
